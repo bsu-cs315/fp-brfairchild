27a3796b9ca5552724c27ae91f649ac327f9c5e4
brfairchild <brfairchild@bsu.edu>
2024-10-29 21:16:02 -0400

    Player no longer can leave the screen.

--- project/player/player.gd
+++ project/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed: float = 425
 @export var focus_speed: float = 175
-@export var tilt_amount: float = 8.0
+@export var tilt_amount: float = 10.0
 
 
 func _process(_delta: float):
@@ -12,11 +12,15 @@ func _process(_delta: float):
 	)
 	
 	var current_speed = focus_speed if Input.is_action_pressed("focus") else speed
-	
 	velocity = direction * current_speed
+	
 	move_and_slide()
 	
 	if direction.y != 0:
 		$AnimatedSprite2D.rotation_degrees = direction.y * tilt_amount
 	else:
 		$AnimatedSprite2D.rotation_degrees = lerp(float($AnimatedSprite2D.rotation_degrees), 0.0, 0.1)
+
+	var screen_size = get_viewport_rect().size
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
